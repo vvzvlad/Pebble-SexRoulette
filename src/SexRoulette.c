3c3aa64c7fba20187d8a94c085c44bf4c58f7602
stantr <github@korylec.de>
2013-12-17 14:54:55 +0100

    German translation

--- src/SexRoulette.c
+++ src/SexRoulette.c
@@ -10,7 +10,7 @@ bool first_time=true; /* создаем флаг первого запуска *
 #define RUS 0
 
 #if (RUS == 0)
-    static const char* messages[] = {"In the bathroom","In the kitchen","On the floor","On the bed","In the toilet","In the corridor","In the friends house","On the balcony","In the closet","In the elevator","In the weather","In the car","In water","In a public toilet","When burning candles","In the bedroom","In the living room","In the fitting room","In the cinema","During video recording","On the beach",}; /* Создаем массив ответов */
+    static const char* messages[] = {"Im Badezimmer","In der Küche","Auf dem Boden","Im Bett","Auf der Toilette","Im Korridor","Im Haus des Freundes","Auf dem Balkon","Auf dem Klo","Im Fahrstuhl","Auf dem Tisch","Im Auto","Im Wasser","Auf einer öffentlichen Toilette","Bei Kerzenschein","Im Schlafzimmer","Im Wohnzimmer","In der Umkleidekabine","Im Kino","Bei laufender Kamera","Am Strand",}; /* German Locations */
 #elif (RUS == 1)
     static const char* messages[] = {"В ванной","На кухне","На полу","На кровати","В туалете","В коридоре","В гостях","На балконе","В шкафу","В лифте","На улице","В машине","В воде","В общественном туалете","При свечах","В спальне","В гостиной","В примерочной кабинке","В кинотеатре","Перед камерой","На пляже",}; /* Создаем массив ответов */
 #endif
@@ -84,7 +84,7 @@ int main(void)
     config_text_layer(0, 10, 144, 168, FONT_KEY_GOTHIC_24); /* настраиваем создание текстового слоя с приветственным сообщением */
 
 #if (RUS == 0)
-    text_layer_set_text(text_layer, "Sex Roulette \n \n Click on any button to select a random pose and place \n ---------------->");  /* показываем сообщение при запуске */
+     text_layer_set_text(text_layer, "Sex Roulette \n Drücke einen beliebigen Knopf für Ort und Stellung -->");  /* German Start-Screen */
 #elif (RUS == 1)
     text_layer_set_text(text_layer, "Sex Roulette \n Нажми на любую кнопку, чтобы выбрать позу и место -->");  /* показываем сообщение при запуске */
 #endif
